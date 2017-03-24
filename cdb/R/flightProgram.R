26e3d25a68976eec6e4d3ac08b3630cae8be8ab6
Douglas Lovell <doug@wbreeze.com>
2017-03-24 15:31:55 -0300

    Test and implement flight grades retrieval

--- cdb/R/flightProgram.R
+++ cdb/R/flightProgram.R
@@ -8,14 +8,38 @@ CDBFlightProgram <- function(url) {
   cfp <- list()
   cfp$url <- url
   class(cfp) <- "CDBFlightProgram"
-  fp <- fromJSON(url)
-  cfp$raw <- fp
+  cfp$raw <- fromJSON(url)
 
-  # gather grades for all of the pilots from all of the judges
-  # together with k values of the sequences and any penalties
-  cfp$collectCDBjf <- function(url) {
-    resource = sprintf("%s/jf_results/%d", "https://iaccdb.iac.org", id)
-    jf <- fromJSON(resource)
+  # Gather all of the judges' grades for all of the pilots in the flight program.
+  cfp$gradesByJudge <- function() {
+    judges <- cfp$raw$flight$line_judges
+    judgeId <- judges$judge$id
+    pfrs <- cfp$raw$flight$pilot_results
+    pfUrls <- pfrs$url
+    pfds <- lapply(pfUrls, function(url) fromJSON(url)[[1]])
+    judgePilotGrades <- lapply(judgeId, function(jid) {
+      vector(mode='integer', length=0)
+    })
+    names(judgePilotGrades) <- judgeId
+    kValues <- c()
+    pilotIds <- c()
+    figureNumbers <- c()
+    for(pfd in pfds) {
+      pid <- pfd$pilot$id
+      seq <- pfd$sequence$k_values
+      pilotIds <- c(recursive=TRUE, pilotIds, rep(pid, length(seq)))
+      kValues <- c(recursive=TRUE, kValues, seq)
+      figureNumbers <- c(recursive=TRUE, figureNumbers, 1:length(seq))
+      grades <- pfd$grades
+      for (i in 1:nrow(grades)) {
+        jg = grades[i,]
+        jid <- as.character(jg$judge$id)
+        judgePilotGrades[[jid]] <- c(recursive=TRUE,
+          judgePilotGrades[[jid]], jg$values)
+      }
+    }
+    return(data.frame(K=kValues, FN=figureNumbers, PN=pilotIds,
+      judgePilotGrades))
   }
 
   return(cfp)
