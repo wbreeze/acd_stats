777e172c83d097a19f7ccdebc8c52534046b7fd6
Douglas Lovell <doug@wbreeze.com>
2019-08-13 16:35:34 -0300

    Revise test to use data set

--- aerobatStats/tests/testthat/testGradesByJudge.R
+++ aerobatStats/tests/testthat/testGradesByJudge.R
@@ -3,21 +3,20 @@ context("Grades by judge")
 
 describe("Read and write grades", {
   setupGradesByJudge <- function() {
-    fp <- CDBFlightProgram("https://iaccdb.iac.org/flights/9014.json")
-    return(GradesByJudge(fp$gradesByJudge()))
+    return(GradesByJudge(grades_IAC_flight_10780))
   }
 
   it("Reads a grade", {
     gbj <- setupGradesByJudge()
-    grade <- getGrade(gbj, 'X1695', 2915, 7)
-    expect_equal(grade, 45)
+    grade <- getGrade(gbj, 'X2414', 1033, 9)
+    expect_equal(grade, 65)
   })
 
   it("Writes a grade", {
     gbj <- setupGradesByJudge()
-    judge <- "X744"
-    pilot <- 2915
-    figure <- 4
+    judge <- "X49"
+    pilot <- 46
+    figure <- 1
     grade <- 0
     gbj <- setGrade(gbj, judge, pilot, figure, grade)
     expect_equal(getGrade(gbj, judge, pilot, figure), grade)
@@ -26,25 +25,24 @@ describe("Read and write grades", {
 
 describe("Zeros and averages", {
   setupZerosAndAverages <- function() {
-    fp <- CDBFlightProgram("https://iaccdb.iac.org/flights/9014.json")
-    gbj <- GradesByJudge(fp$gradesByJudge())
-    gbj <- setGrade(gbj, 'X1695', 2915, 2, -10)
-    gbj <- setGrade(gbj, 'X744', 2915, 2, -20)
-    gbj <- setGrade(gbj, 'X657', 2915, 2, -10)
-    gbj <- setGrade(gbj, 'X1695', 2915, 3, -30)
-    gbj <- setGrade(gbj, 'X744', 2915, 3, -30)
-    gbj <- setGrade(gbj, 'X657', 2915, 3, 0)
-    gbj <- setGrade(gbj, 'X1695', 2915, 4, -30)
-    gbj <- setGrade(gbj, 'X744', 2915, 4, -30)
-    gbj <- setGrade(gbj, 'X1695', 2915, 5, -30)
-    gbj <- setGrade(gbj, 'X744', 2915, 5, -30)
-    gbj <- setGrade(gbj, 'X657', 2915, 5, -30)
-    gbj <- setGrade(gbj, 'X1695', 2915, 6, -30)
-    gbj <- setGrade(gbj, 'X744', 2915, 6, -30)
-    gbj <- setGrade(gbj, 'X657', 2915, 6, -10)
-    gbj <- setGrade(gbj, 'X1695', 2915, 7, -30)
-    gbj <- setGrade(gbj, 'X744', 2915, 7, -30)
-    gbj <- setGrade(gbj, 'X657', 2915, 7, -20)
+    gbj <- GradesByJudge(grades_IAC_flight_10780)
+    gbj <- setGrade(gbj, 'X3045', 2448, 2, -10)
+    gbj <- setGrade(gbj, 'X49', 2448, 2, -20)
+    gbj <- setGrade(gbj, 'X2414', 2448, 2, -10)
+    gbj <- setGrade(gbj, 'X3045', 2448, 3, -30)
+    gbj <- setGrade(gbj, 'X49', 2448, 3, -30)
+    gbj <- setGrade(gbj, 'X2414', 2448, 3, 0)
+    gbj <- setGrade(gbj, 'X3045', 2448, 4, -30)
+    gbj <- setGrade(gbj, 'X49', 2448, 4, -30)
+    gbj <- setGrade(gbj, 'X3045', 2448, 5, -30)
+    gbj <- setGrade(gbj, 'X49', 2448, 5, -30)
+    gbj <- setGrade(gbj, 'X2414', 2448, 5, -30)
+    gbj <- setGrade(gbj, 'X3045', 2448, 6, -30)
+    gbj <- setGrade(gbj, 'X49', 2448, 6, -30)
+    gbj <- setGrade(gbj, 'X2414', 2448, 6, -10)
+    gbj <- setGrade(gbj, 'X3045', 2448, 7, -30)
+    gbj <- setGrade(gbj, 'X49', 2448, 7, -30)
+    gbj <- setGrade(gbj, 'X2414', 2448, 7, -20)
     gbj <- resolveZerosAndAverages(gbj)
     return(gbj)
   }
