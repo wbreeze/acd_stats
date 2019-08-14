cb9d6d7f23132a7742b1fda60a438ec4322d2558
Douglas Lovell <doug@wbreeze.com>
2019-08-14 19:16:11 -0300

    Add first pass for Chi-sq GOF test

--- aerobatStats/R/judgeGradeDistribution.R
+++ aerobatStats/R/judgeGradeDistribution.R
@@ -1,6 +1,8 @@
 # Analyze judge grade distributions for each judge and each figure
 # fp is Flight Program data in form returned by
 #    CDBFlightProgram::gradesByJudge
+# zeroNA, if set FALSE, will override default conversion of
+#    all zero-ish and average values to NA
 #
 #     K FN   PN X1695 X744 X657 X456
 # 1  10  1 2915    65   80   70   80
@@ -17,14 +19,73 @@
 # FN: figure number
 # PN: pilot id number
 # X1695 X744 X657 X456: grades from four judges
-JudgeGradeDistribution <- function(fp) {
+JudgeGradeDistribution <- function(fp, zeroNA=TRUE) {
   jgd <- list()
   jgd$fp <- fp
+  if(zeroNA) {
+    jgd$fp[jgd$fp <= 0] <- NA
+  }
   class(jgd) <- "JudgeGradeDistribution"
 
   # Subset data for a given figure number
   jgd$figureGrades <- function(figure_number) {
     subset(jgd$fp, jgd$fp$FN == figure_number)
+  }
+
+  # Count number of occurrences within the range of grade values
+  #   encompassed by the grades
+  # This is something like, table(), but includes zero counts and
+  #   also returns the range
+  # Returns a list with range and counts elements
+  jgd$distribution <- function(grades) {
+    g <- grades[!is.na(grades)]
+    r <- seq(min(g), max(g), 5)
+    fct <- function(X, g) {
+      length(g[g==X])
+    }
+    ct <- sapply(r, fct, g)
+    list(range=r, counts=ct)
+  }
+
+  # Subset the data into groups according to FPS 3.3
+  # Returns a list or vector of data frames that match the format of fp
+  jgd$groups <- function() {
+    jgd$fp
+  }
+
+  # Determine the intervals for Chi-square goodness of fit
+  #   and the counts within them.
+  # The intervals must encompass five or more grades.
+  # Where there are fewer than five grades of a given value,
+  #   the interval is extended to encompass more than one value.
+  # TODO
+  jgd$intervals <- function(grades) {
+    g <- grades[!is.na(grades)]
+    c(-Inf, seq(min(g)+2.5, max(g)-2.5, 5), Inf)
+  }
+
+  # Compute normal distribution for a collection of judge grades
+  jgd$normal_distribution <- function(grades) {
+    g <- grades[!is.na(grades)]
+    diff(pnorm(jgd$intervals(grades), mean(g), sd(g)))
+  }
+
+  # Chi-square probability of fit of collection of judge grades to normal
+  jgd$chiSqP <- function(grades) {
+    g <- grades[!is.na(grades)]
+    t <- jgd$distribution(g)
+    chisq.test(t$counts, p=jgd$normal_distribution(g))$p.value
+  }
+
+  # Plot grade frequency histogram overlayed with the derived normal curve
+  jgd$densityPlot <- function(grades) {
+    g <- grades[!is.na(grades)]
+    cutp <- seq(min(g)-2.5, max(g)+2.5, 5)
+    d <- jgd$distribution(g)
+    x <- rep(d$range, times=d$counts)
+    hist(x, prob=T, br=cutp, col="skyblue2",
+      xlim=c(0,100), ylim=c(0,.06))
+      curve(dnorm(x, mean(g), sd(g)), add=T, lwd=2, col="red")
   }
 
   return(jgd)
--- aerobatStats/tests/testthat/testJudgeGradeDistribution.R
+++ aerobatStats/tests/testthat/testJudgeGradeDistribution.R
@@ -2,8 +2,7 @@ context("Judge grade distribution")
 
 describe("Process flight program", {
   setupJudgeGradeDistribution <- function() {
-    fp <- CDBFlightProgram("https://iaccdb.iac.org/flights/9014.json")
-    return(JudgeGradeDistribution(fp$gradesByJudge()))
+    return(JudgeGradeDistribution(grades_IAC_flight_10780))
   }
 
   test_that("creates class from grades by judge", {
@@ -12,11 +11,71 @@ describe("Process flight program", {
     expect_s3_class(jgd, "JudgeGradeDistribution")
   })
 
+  test_that("changes zero-ish values and averages to NA", {
+    jgd <- JudgeGradeDistribution(grades_IAC_flight_10780)
+    expect_gt(nrow(subset(jgd$fp, is.na(jgd$fp))), 0)
+    expect_equal(nrow(subset(jgd$fp, jgd$fp == 0)), 0)
+    expect_equal(nrow(subset(jgd$fp, jgd$fp == -30)), 0)
+    expect_equal(nrow(subset(jgd$fp, jgd$fp == -10)), 0)
+  })
+
+  test_that("leaves zero-ish values and averages when zeroNA false", {
+    jgd <- JudgeGradeDistribution(grades_IAC_flight_10780, zeroNA=FALSE)
+    expect_equal(nrow(subset(jgd$fp, is.na(jgd$fp))), 0)
+    expect_gt(nrow(subset(jgd$fp, jgd$fp == 0)), 0)
+    expect_gt(nrow(subset(jgd$fp, jgd$fp == -30)), 0)
+    expect_gt(nrow(subset(jgd$fp, jgd$fp == -10)), 0)
+  })
+
   test_that("subsets rows for one figure", {
     jgd <- setupJudgeGradeDistribution()
     f1s <- jgd$figureGrades(1)
     expect_false(is.null(f1s))
-    expect_length(f1s$PN, 2)
+    expect_length(f1s$PN, 10)
     expect_equal(unique(f1s$FN), 1)
+  })
+
+  test_that("provides normal distribution for set of grades", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- jgd$figureGrades(2)$X49
+    expect_equal(min(grades), 75)
+    expect_equal(max(grades), 90)
+    dist <- jgd$normal_distribution(grades)
+    expect_equal(round(dist * 100), c(36, 35, 22, 7))
+  })
+
+  test_that("provides normal distribution with presence of NA", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- jgd$fp$X49
+    nd <- round(jgd$normal_distribution(grades) * 1000)
+    expect_equal(nd, c(3, 18, 71, 177, 270, 253, 145, 64))
+  })
+
+  test_that("normal distribution probabilities sum to one", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- jgd$fp$X49
+    nd <- jgd$normal_distribution(grades)
+    expect_equal(sum(nd), 1.0)
+  })
+
+  test_that("provides chi-square fit to normal", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- jgd$figureGrades(2)$X49
+    expect_equal(round(jgd$chiSqP(grades) * 1000), 725)
+  })
+
+  test_that("provides chi-square fit to normal with presence of NA", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- jgd$fp$X49
+    pValue <- round(jgd$chiSqP(grades) * 1000)
+    expect_equal(pValue, 413)
+  })
+
+  test_that("counts zero frequency within the range", {
+    jgd <- setupJudgeGradeDistribution()
+    grades <- c(55, 60, 60, rep(70,4), NA, rep(75,6), 80, 80, 85)
+    t <- jgd$distribution(grades)
+    expect_equal(t$range, seq(55,85,5))
+    expect_equal(t$counts, c(1, 2, 0, 4, 6, 2, 1))
   })
 })
