13273b4b7ced19d9882497168d54698d58aae954
Douglas Lovell <doug@wbreeze.com>
2019-08-23 11:22:46 -0300

    Complete test and groom arguments for Prechi#solve

--- prechi/R/preChi.R
+++ prechi/R/preChi.R
@@ -1,18 +1,30 @@
-# Expects a list containing grade values and counts
-# grades: vector of integer grade values, increasing
+# Join neighboring clusters with fewer than minimim_count grades
+# grades: vector of numeric grade values, increasing
 # counts: vector of integer counts
+# minimum_count: integer minimum size of a cluster, defaults to five
 prechi.cluster_neighbors <- function(grades, counts, minimum_count = 5) {
-  min_ct <- max(as.integer(2), as.integer(minimum_count))
+  n <- as.integer(minimum_count)
+  ln <- length(n)
+  if (1 < ln) {
+    warning("Prechi: ", ln,
+      " elements given for minimum count, using the first")
+    n <- n[1]
+  }
+  n_min <- as.integer(2)
+  if (n < n_min) {
+    warning("Prechi: minimum count of ", n, " increased to ", n_min)
+    n <- n_min
+  }
   g <- as.numeric(grades)
   c <- as.integer(counts)
   gl <- length(g)
   cl <- length(c)
   if (gl != cl) {
     l <- min(gl, cl)
-    warning("Unequal length arrays, only first ", l, " elements used")
+    warning("Prechi: Unequal length arrays, only first ", l, " elements used")
     g <- g[0:l]
-    c <- g[0:l]
+    c <- c[0:l]
   }
 
-  .Call(pre_chi_cluster_neighbors, g, c, min_ct)
+  .Call(pre_chi_cluster_neighbors, g, c, n)
 }
--- prechi/test/testthat/testPreChi.R
+++ prechi/test/testthat/testPreChi.R
@@ -5,22 +5,59 @@ test_that("computes clusters", {
   grades <- c(55, 60, 65, 70, 75, 80, 85)
   counts <- c( 2,  0,  5,  8, 12, 10,  4)
   clustered <- prechi.cluster_neighbors(grades, counts)
-  expect_false(is.null(clustered))
   str(clustered)
+  expect_false(is.null(clustered))
 })
 
 test_that("too few grades", {
   grades <- c(55, 60, 65, 70, 75, 80)
   counts <- c( 2,  0,  5,  8, 12, 10,  4)
-  clustered <- prechi.cluster_neighbors(grades, counts)
-  expect_false(is.null(clustered))
+  expect_warning(clustered <- prechi.cluster_neighbors(grades, counts),
+    "Prechi: Unequal length arrays")
   str(clustered)
+  expect_false(is.null(clustered))
 })
 
 test_that("too few counts", {
   grades <- c(55, 60, 65, 70, 75, 80, 85)
   counts <- c( 2,  0,  5,  8, 12, 10)
-  clustered <- prechi.cluster_neighbors(grades, counts)
-  expect_false(is.null(clustered))
+  expect_warning(clustered <- prechi.cluster_neighbors(grades, counts),
+    "Prechi: Unequal length arrays")
   str(clustered)
+  expect_false(is.null(clustered))
+})
+
+test_that("grades and counts the same", {
+  grades <- c(55, 60, 65, 70, 75, 80, 85)
+  clustered <- prechi.cluster_neighbors(grades, grades)
+  str(clustered)
+  expect_false(is.null(clustered))
+})
+
+test_that("minimum too low", {
+  grades <- c(55, 60, 65, 70, 75, 80)
+  counts <- c( 2,  0,  5,  8, 12, 10)
+  expect_warning(clustered <- prechi.cluster_neighbors(grades, counts, 1),
+    "Prechi: minimum count of 1 increased")
+  str(clustered)
+  expect_false(is.null(clustered))
+})
+
+test_that("more than one minimum", {
+  grades <- c(55, 60, 65, 70, 75, 80, 85)
+  counts <- c( 2,  0,  5,  8, 12, 10,  4)
+  minimum_count <- c(5, 0, 0)
+  expect_warning(
+    clustered <- prechi.cluster_neighbors(grades, counts, minimum_count),
+    "Prechi: 3 elements given for minimum count")
+  str(clustered)
+  expect_false(is.null(clustered))
+})
+
+test_that("too few parts", {
+  grades <- c()
+  counts <- c()
+  clustered <- prechi.cluster_neighbors(grades, grades)
+  str(clustered)
+  expect_false(is.null(clustered))
 })
