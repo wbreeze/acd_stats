7ad9d3fd78b6021298fc5e231d53f2f5f07a19bd
Douglas Lovell <doug@wbreeze.com>
2019-09-12 13:43:55 -0300

    Remove class-like wrapper around densityPlot function

--- aerobatStats/R/judgeGradeDistribution.R
+++ aerobatStats/R/judgeGradeDistribution.R
@@ -1,42 +1,34 @@
 # Analyze judge grade distributions for each judge and each figure
-# fp is Flight Program data in form returned by
-#    CDBFlightProgram::gradesByJudge
-# zeroNA, if set FALSE, will override default conversion of
-#    all zero-ish and average values to NA
+# id: flight identifier
+# class: flight competition class, e.g. glider 'G' or power 'P'
+# category: flight competition category, e.g. "Unlimited"
+# format: is the flight program format, e.g. "Known", "Unknown"
+# fp: Flight Program data in form returned by
+#   CDBFlightProgram::gradesByJudge, like this:
 #
-#     K FN   PN X1695 X744 X657 X456
-# 1  10  1 2915    65   80   70   80
-# 2  14  2 2915    70   75   75   85
-# ...
-# 7   3  7 2915    45   70   65   80
-# 8  10  1 1415    90   90   85   80
-# 9  14  2 1415    90   90   85   85
-# ...
-# 14  3  7 1415    90   80   90   80
-# ...
+#         K FN   PN X1695 X744 X657 X456
+#     1  10  1 2915    65   80   70   80
+#     2  14  2 2915    70   75   75   85
+#     ...
+#     7   3  7 2915    45   70   65   80
+#     8  10  1 1415    90   90   85   80
+#     9  14  2 1415    90   90   85   85
+#     ...
+#     14  3  7 1415    90   80   90   80
 #
-# K: figure difficulty
-# FN: figure number
-# PN: pilot id number
-# X1695 X744 X657 X456: grades from four judges
-JudgeGradeDistribution <- function(fp, zeroNA=TRUE) {
-  jgd <- list()
-  jgd$fp <- fp
-  if(zeroNA) {
-    jgd$fp[jgd$fp <= 0] <- NA
-  }
-  class(jgd) <- "JudgeGradeDistribution"
+#     K: figure difficulty
+#     FN: figure number
+#     PN: pilot id number
+#     X1695 X744 X657 X456: grades from four judges
+jgd.processFlight <- function(id, class, category, format, fp) {
+}
 
-  # Plot grade frequency histogram overlayed with the derived normal curve
-  jgd$densityPlot <- function(grades) {
-    g <- grades[!is.na(grades)]
-    cutp <- seq(min(g)-2.5, max(g)+2.5, 5)
-    d <- jgd$gradeCounts(g)
-    x <- rep(d$range, times=d$counts)
-    hist(x, prob=T, br=cutp, col="skyblue2",
-      xlim=c(0,100), ylim=c(0,.06))
-      curve(dnorm(x, mean(g), sd(g)), add=T, lwd=2, col="red")
-  }
-
-  return(jgd)
+# Plot grade frequency histogram overlayed with the derived normal curve
+jgd.densityPlot <- function(gradeCounts) {
+  g <- gradeCounts.grades
+  cutp <- seq(min(g)-2.5, max(g)+2.5, 5)
+  x <- rep(gradeCounts$range, times=gradeCounts$counts)
+  hist(x, prob=T, br=cutp, col="skyblue2",
+    xlim=c(0,100), ylim=c(0,.06))
+    curve(dnorm(x, mean(g), sd(g)), add=T, lwd=2, col="red")
 }
--- aerobatStats/tests/testthat/testJudgeGradeDistribution.R
+++ aerobatStats/tests/testthat/testJudgeGradeDistribution.R
@@ -1,29 +1,4 @@
 context("Judge grade distribution")
 
 describe("Process flight program", {
-  setupJudgeGradeDistribution <- function() {
-    return(JudgeGradeDistribution(grades_IAC_flight_10780))
-  }
-
-  test_that("creates class from grades by judge", {
-    jgd <- setupJudgeGradeDistribution()
-    expect_false(is.null(jgd))
-    expect_s3_class(jgd, "JudgeGradeDistribution")
-  })
-
-  test_that("changes zero-ish values and averages to NA", {
-    jgd <- JudgeGradeDistribution(grades_IAC_flight_10780)
-    expect_gt(nrow(subset(jgd$fp, is.na(jgd$fp))), 0)
-    expect_equal(nrow(subset(jgd$fp, jgd$fp == 0)), 0)
-    expect_equal(nrow(subset(jgd$fp, jgd$fp == -30)), 0)
-    expect_equal(nrow(subset(jgd$fp, jgd$fp == -10)), 0)
-  })
-
-  test_that("leaves zero-ish values and averages when zeroNA false", {
-    jgd <- JudgeGradeDistribution(grades_IAC_flight_10780, zeroNA=FALSE)
-    expect_equal(nrow(subset(jgd$fp, is.na(jgd$fp))), 0)
-    expect_gt(nrow(subset(jgd$fp, jgd$fp == 0)), 0)
-    expect_gt(nrow(subset(jgd$fp, jgd$fp == -30)), 0)
-    expect_gt(nrow(subset(jgd$fp, jgd$fp == -10)), 0)
-  })
 })
