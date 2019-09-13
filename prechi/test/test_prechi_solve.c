864084ddab5156399747adf3bbbc75c6867ba09c
Douglas Lovell <doug@wbreeze.com>
2019-09-13 16:28:35 -0300

    Test a problem that takes a long time

--- prechi/test/test_prechi_solve.c
+++ prechi/test/test_prechi_solve.c
@@ -72,6 +72,30 @@ void test_prechi_solve_3(void) {
   destroy_test_data(td);
 }
 
+void test_prechi_solve_4(void) {
+  int n = 15;
+  TestData *td = create_test_data(n);
+  int_array_init(td->counts, n, 2, 0, 0, 0, 3, 0, 0, 0, 2, 3, 5, 1, 4, 2, 8);
+  Prechi *prechi = prechi_create(td->dweights, td->counts, n);
+
+  int expected_parts = 5;
+  prechi_solve(prechi, 5);
+  cut_assert_equal_int(expected_parts, prechi->solution_part_count);
+
+  //re-use counts for span check
+  int_array_init(td->counts, expected_parts, 5, 5, 5, 7, 8);
+  assert_equal_int_arrays(
+    td->counts, prechi->solution_spans, expected_parts);
+
+  assert_equal_float(td->weights[4] + 2.5, prechi->solution_boundaries[0], 2);
+  assert_equal_float(td->weights[9] + 2.5, prechi->solution_boundaries[1], 2);
+  assert_equal_float(td->weights[10] + 2.5, prechi->solution_boundaries[2], 2);
+  assert_equal_float(td->weights[13] + 2.5, prechi->solution_boundaries[3], 2);
+
+  prechi_destroy(prechi);
+  destroy_test_data(td);
+}
+
 void test_prechi_solve_no_solution(void) {
   int n = 7;
   TestData *td = create_test_data(n);
