3fab04f6430ff2d7bedaa207d941355758c7fb30
Douglas Lovell <doug@wbreeze.com>
2019-11-15 16:16:12 -0300

    Setup references and bibliography

--- jgdfit/texit.sh
+++ jgdfit/texit.sh
@@ -1,4 +1,9 @@
 mkdir -p texout
 latex -interaction=nonstopmode -draftmode -output-directory=texout main.tex
+cp references.bib texout
+cd texout
+bibtex main
+cd ..
+latex -interaction=nonstopmode -draftmode -output-directory=texout main.tex
 latex -interaction=nonstopmode -output-format=pdf -output-directory=texout main.tex
 mv texout/main.pdf .
