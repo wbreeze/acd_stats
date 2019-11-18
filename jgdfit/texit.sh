fa71b7e893f83b41188a5b60339e8033192f9b31
Douglas Lovell <doug@wbreeze.com>
2019-11-18 18:33:48 -0300

    Rename output pdf

--- jgdfit/texit.sh
+++ jgdfit/texit.sh
@@ -6,4 +6,4 @@ bibtex main
 cd ..
 latex -interaction=nonstopmode -draftmode -output-directory=texout main.tex
 latex -interaction=nonstopmode -output-format=pdf -output-directory=texout main.tex
-mv texout/main.pdf .
+mv texout/main.pdf jgdfit201911.pdf
